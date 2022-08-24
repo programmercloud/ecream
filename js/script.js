ab6a9bc67ac6083ef6201dc49bd361d33bfd3479
programmercloud <88435015+programmercloud@users.noreply.github.com>
2022-08-24 15:39:19 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,65 @@
+/* ----- BASE ------ */
+"use strict";
+
+// get required selectors to maniplute menu toggle
+const navbar = document.querySelector(".navbar");
+const menuTogglersContainer = document.querySelector(".menu-togglers");
+const bxMenu = document.querySelector(".bx-menu");
+
+/* -- show/hide menu -- */
+menuTogglersContainer.addEventListener("click", () => {
+  navbar.classList.toggle("show-nav");
+});
+
+/* -------- theme changing -------- */
+const themeTogglers = document.querySelector(".theme-togglers");
+const lightIcon = document.querySelector(".bxs-sun");
+const darkIcon = document.querySelector(".bxs-moon");
+
+var lightmode = localStorage.getItem("lightmode");
+
+// enable dark mode function
+const enableLightMode = () => {
+  // add class dark mode to the body
+  document.body.classList.add("lightmode");
+  localStorage.setItem("lightmode", "enabled");
+  // change theme toggle styles
+  lightIcon.style.display = "none";
+  darkIcon.style.display = "block";
+};
+
+if (lightmode && lightmode === "enabled") {
+  enableLightMode();
+}
+
+// disable dark mode function
+const disableLightMode = () => {
+  // remove class dark mode from the body
+  document.body.classList.remove("lightmode");
+  localStorage.setItem("lightmode", null);
+  // change theme toggle styles
+  lightIcon.style.display = "block";
+  darkIcon.style.display = "none";
+};
+
+// active/deactive dark mode
+themeTogglers.addEventListener("click", () => {
+  lightmode = localStorage.getItem("lightmode");
+  if (!lightmode || lightmode !== "enabled") {
+    enableLightMode();
+  } else {
+    disableLightMode();
+  }
+});
+
+/* -- hide show hero buttons -- */
+// delay before showing them
+const heroButtonsContainer = document.querySelector(".hero-btns-container");
+
+var delayTime = 1000;
+
+heroButtonsContainer.style.transition = "opacity 1000ms";
+
+setTimeout(() => {
+  heroButtonsContainer.style.opacity = 1;
+}, delayTime);
